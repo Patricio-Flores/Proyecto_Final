7c79e9f999601c570c7ec298a2fa3bdddb30c1e1
Pc Usuario <Pc Usuario@DESKTOP-LTH9DTB>
2021-05-31 20:04:33 -0500

    cambio jhonatan poligono

--- Trabajo_en_Grupo/src/main/java/com/mycompany/trabajo_en_grupo/poligono.java
+++ Trabajo_en_Grupo/src/main/java/com/mycompany/trabajo_en_grupo/poligono.java
@@ -1,28 +1,26 @@
-
 package com.mycompany.trabajo_en_grupo;
 
-
 public class poligono {
-    // variables 
-    boolean verificar =false;
-        private double apotema;
-    private double numerol;
-    private double lado ; 
-    private double perimetro;
-    public poligono(double ap, double num, double l){
-        this.apotema = ap;
-        this.numerol =num;
-        this.lado = l;
-        
-    }
-    public double perimetro(){
-        this.perimetro = this.lado *this.numerol;
-        return this.lado *this.numerol;
-        
-    }
-    public double area(){
-        return ((this.perimetro)*this.apotema)/2;
-    }
-    
-}
 
+    // variables 
+    boolean verificar = false;
+    private double apotema;
+    private double numerol;
+    private double lado;
+    private double perimetro;
+
+    public poligono(double ap, double num, double l) {
+        this.apotema = ap;
+        this.numerol = num;
+        this.lado = l;
+
+    }
+    public double perimetro() {
+        this.perimetro = this.lado * this.numerol;
+        return this.lado * this.numerol;
+
+   }
+    public double area() {
+        return ((this.perimetro) * this.apotema) / 2;
+    }
+}
